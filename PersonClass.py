30a1fc406769f9ffbee357a195c447da12f51285
Korbin Scott <korbin_scott1@baylor.edu>
2023-02-22 15:00:53 -0600

    2/22

--- PersonClass.py
+++ PersonClass.py
@@ -1,14 +1,37 @@
 class Person:
     def __init__(self, name, address, phone):
-        self.__name = name
-        self.__address = address
-        self.__phone = phone
+        self.name = name
+        self.address = address
+        self.phone = phone
+
+    def get_name(self):
+        return self.name
+
+    def get_address(self):
+        return self.address
+
+    def get_phone_number(self):
+        return self.phone
 
     def print_person(self):
-        print()
+        print("Name:", self.name)
+        print("Addr:", self.address)
+        print("Phone:", self.phone)
 
 
 class Customer(Person):
-    def __init__(self):
-        Person.__init__(self, "Customer")
-        Person.__init__
+    def __init__(self, name, address, phone, cust_number, on_list):
+        Person.__init__(self, name, address, phone)
+
+        self.cust_number = cust_number
+        self.on_list = on_list
+
+    def print_person(self):
+        print("Name:", self.name)
+        print("Addr:", self.address)
+        print("Phone:", self.phone)
+        print("Customer Number:", self.cust_number)
+        if self.on_list:
+            print("On Mailing List: Yes")
+        else:
+            print("On Mailing List: No")
